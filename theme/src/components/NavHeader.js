8d9d5e590b298d46f8ee289e0345bfb51e7d6b09
Ramon <Rahmon@users.noreply.github.com>
2019-07-14 12:25:58 -0300

    add NavHeader component

---
+++ theme/src/components/NavHeader.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const NavHeader = styled.nav`
+     margin: 1.625rem 0; 
+
+     & > ul {
+         margin: 0;
+         padding: 0;
+         list-style: none;
+
+         & > li {
+            display: inline-block;
+            margin-right: 1.125rem;
+            margin-bottom: .28rem;
+         }
+     }
+     
+`;
+ 
+export default NavHeader;
