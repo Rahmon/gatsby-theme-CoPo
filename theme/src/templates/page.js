910f25484ff17a4da36bf7dd9259afbd00d2f3ae
Ramon <Rahmon@users.noreply.github.com>
2019-07-14 12:26:54 -0300

    wrap with Layout component

--- theme/src/templates/page.js
+++ theme/src/templates/page.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/layout';
 
 const PageTemplate = ({ pageContext }) => (
-  <>
+  <Layout>
     <h1>{pageContext.heading}</h1>
     <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
-  </>
-)
+  </Layout>
+);
 
-export default PageTemplate
+export default PageTemplate;
