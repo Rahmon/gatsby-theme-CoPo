5ddaae6609015e2ce6d125cf2a4c3c3f9f5199d7
Ramon <Rahmon@users.noreply.github.com>
2019-07-22 14:34:34 -0300

    add components to header, nav, title and subtitle

---
+++ theme/src/components/Header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+    position: relative;
+    margin: 0 auto;
+    margin-bottom: 3.5rem;
+    max-width: 40rem;
+`;
+
+export default Header;
---
+++ theme/src/components/SubtitleHeader.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const SubtitleHeader = styled.p`
+     margin: 0; 
+     font-size: 1rem;
+`;
+ 
+export default SubtitleHeader;
