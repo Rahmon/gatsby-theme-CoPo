6c1ddaf39489f7a2b8d6e5a2941622f6013e8730
Ramon <Rahmon@users.noreply.github.com>
2019-07-23 14:35:28 -0300

    refactor to use Post component

--- theme/src/templates/blog-index.js
+++ theme/src/templates/blog-index.js
@@ -1,31 +1,33 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+
 import Layout from '../components/layout.js'
+import Post from '../components/Post'
 
 const BlogIndex = ({ data }) => {
   const { edges: posts } = data.allMdx
 
   return (
     <Layout>
-      <h1>Awesome MDX Blog</h1>
-
-      <ul>
-        {posts.map(({ node: post }) => (
-          <li key={post.id}>
-            <Link to={post.fields.slug}>
-              <h2>{post.frontmatter.title}</h2>
-            </Link>
-            {/* <p>{post.excerpt}</p> */}
-          </li>
-        ))}
-      </ul>
+      {posts.map(({ node: post }) => (
+        <Post key={post.id}>
+          <header>
+            <h1 style={{fontSize: '2.5rem', margin: '0', lineHeight: '1.2'}}>
+              <Link to={post.fields.slug}>
+                {post.frontmatter.title}
+              </Link>
+            </h1>
+          </header>
+          <p>{post.excerpt}</p>
+        </Post>
+      ))}
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
   query blogIndex {
-    allMdx {
+    allMdx (sort: {fields: frontmatter___date, order: DESC}) {
       edges {
         node {
           id
