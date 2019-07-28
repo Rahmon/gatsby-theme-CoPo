3e3f019560fb801e0bfce68c20f5487221378126
Ramon <Rahmon@users.noreply.github.com>
2019-07-28 14:52:40 -0300

    update font of subtitle

--- theme/src/components/SubtitleHeader.js
+++ theme/src/components/SubtitleHeader.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const SubtitleHeader = styled.p`
      margin: 0; 
      font-size: 1rem;
+     font-family: monospace;
 `;
  
 export default SubtitleHeader;
