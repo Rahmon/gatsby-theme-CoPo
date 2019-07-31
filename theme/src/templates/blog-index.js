5348d4402dafe1c4b0a55b64a06b70651f7bae67
Ramon <Rahmon@users.noreply.github.com>
2019-07-31 08:49:57 -0300

    refactor to use ListOfPosts component

--- theme/src/templates/blog-index.js
+++ theme/src/templates/blog-index.js
@@ -1,28 +1,14 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-import moment from 'moment';
 
-import Layout from '../components/layout.js'
-import Post from '../components/Post'
+import Layout from './layout.js'
+import ListOfPosts from '../components/ListOfPosts'
 
 const BlogIndex = ({ data }) => {
   const { edges: posts } = data.allMdx
 
   return (
     <Layout>
-      {posts.map(({ node: post }) => (
-        <Post key={post.id}>
-          <header>
-            {post.frontmatter.date && <p style={{marginTop: 0,color: '#696969', fontSize: '1rem'}}>{moment(post.frontmatter.date, "MM-DD-YYYY").format('LL')}</p>}
-            <h1 style={{marginTop: '-.703125rem'}}>
-              <Link to={post.fields.slug}>
-                {post.frontmatter.title}
-              </Link>
-            </h1>
-          </header>
-          <p>{post.excerpt}</p>
-        </Post>
-      ))}
+      <ListOfPosts data={posts} />
     </Layout>
   )
 }
