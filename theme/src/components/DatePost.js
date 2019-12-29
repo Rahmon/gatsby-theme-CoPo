2af73bfe8bbc42e1a851ff1592f38767e6fdafb3
Ramon <Rahmon@users.noreply.github.com>
2019-12-29 07:12:25 -0300

    update to format date with language config

--- theme/src/components/DatePost.js
+++ theme/src/components/DatePost.js
@@ -1,19 +1,36 @@
-import React from "react"
-import moment from 'moment';
-import styled from 'styled-components';
+import React from "react";
+import moment from "moment";
+import styled from "styled-components";
+import get from "lodash/get";
+import { graphql, useStaticQuery } from "gatsby";
+import "moment/min/locales.min";
 
 const DatePost = props => {
   const { children: date } = props;
-  
-  const dateFormated = date && moment(date, "YYYY-MM-DD").format('LL');
 
-  return dateFormated && <DatePostStyled>{dateFormated}</DatePostStyled>
-}
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          language
+        }
+      }
+    }
+  `);
+
+  const language = get(data, "site.siteMetadata.language");
+
+  moment.locale(language);
+
+  const dateFormated = date && moment(date, "YYYY-MM-DD").format("LL");
+
+  return dateFormated && <DatePostStyled>{dateFormated}</DatePostStyled>;
+};
 
 const DatePostStyled = styled.p`
   margin-top: 0;
   color: #696969;
-  font-size: 1rem
+  font-size: 1rem;
 `;
 
 export default DatePost;
